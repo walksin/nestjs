3ebf84b192d20e491e729f55126129861d1ea5c2
longfan <longfan@dayuwuxian.com>
2019-08-29 08:00:29 +0800

    config mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { from } from 'rxjs';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'password',
+      database: 'nest',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.ts']
+    }),
+    PostModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
