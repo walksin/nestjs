bcb5727329a31feb3ef9742cbd3a77b971dabe6b
longfan <longfan@dayuwuxian.com>
2019-08-30 17:06:15 +0800

    [301] Store entity from post

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @InjectRepository(Post)
+        private readonly postRepository: Repository<Post>
+    ) { }
+
+    async store(data) {
+        const entity = await this.postRepository.create(data);
+        await this.postRepository.save(entity);
+        return entity;
+    }
+}
