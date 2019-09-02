a77d8658dd63cbe79894804daac8c0093f60d26e
longfan <longfan@dayuwuxian.com>
2019-09-03 07:34:25 +0800

    store username and password

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { async } from 'rxjs/internal/scheduler/async';
+import { UserDto } from './user.dto';
 
 @Controller('users')
 export class UserController {
     constructor(
         private readonly service: UserService
     ) { }
+
+    @Post()
+    async store(@Body() data: UserDto) {
+        return await this.service.store(data);
+    }
 }
