4509ee7f6121afe2a6ebdb870ed6a9c770b4ba80
longfan <longfan@dayuwuxian.com>
2019-09-03 08:57:10 +0800

    auth dependent user service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
 
 @Module({
+  imports: [AuthService],
   controllers: [AuthController],
   providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
