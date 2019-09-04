d06b4724bfe97ceb853de5d0333068fa5da37497
longfan <longfan@dayuwuxian.com>
2019-09-04 19:57:16 +0800

    [310] login function

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './auth.dto';
+
 
 @Controller('auth')
 export class AuthController {
     constructor(
         private readonly service: AuthService
     ) { }
+
+    @Post('login')
+    @UseInterceptors(ClassSerializerInterceptor)
+    async login(@Body() data: LoginDto) {
+        return await this.service.login(data);
+    }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { LoginDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
-   constructor(
-       private readonly userService:UserService
-   ){}
+    constructor(
+        private readonly userService: UserService
+    ) { }
+
+    async login(data: LoginDto) {
+        const { name, password } = data;
+        const entity = await this.userService.findByName(name);
+        if (!entity) {
+            throw new UnauthorizedException('user not exist');
+        }
+        if (!(await entity.comparePassword(password))) {
+            throw new UnauthorizedException('password wrong');
+        }
+        return entity;
+    }
 }
