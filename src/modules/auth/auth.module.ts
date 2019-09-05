5385e02db459b562b3b40630c3abf85eda73705a
longfan <longfan@dayuwuxian.com>
2019-09-05 12:04:26 +0800

    [311] Token with JwtService

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt'
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    JwtModule.register({
+      secretOrPrivateKey: 'qhtsdfsadfafsdfadfaddsadfasdfadadfadffa',
+      signOptions: {
+        expiresIn: '12h'
+      }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { LoginDto } from './auth.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './auth.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private readonly userService: UserService
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
     ) { }
 
     async login(data: LoginDto) {
@@ -17,6 +20,17 @@ export class AuthService {
         if (!(await entity.comparePassword(password))) {
             throw new UnauthorizedException('password wrong');
         }
-        return entity;
+        const { id } = entity;
+        const payload = { id, name }
+        const token = this.signToken(payload);
+
+        return {
+            ...payload,
+            token
+        }
+    }
+
+    signToken(data: JwtPayload) {
+        return this.jwtService.sign(data);
     }
 }
