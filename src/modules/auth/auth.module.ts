d3e7f67ea185ba4b644ebf94ec9e84389685fed8
longfan <longfan@dayuwuxian.com>
2019-09-05 13:35:52 +0800

    [312] Passport with Jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { UserModule } from '../user/user.module';
+import { from } from 'rxjs';
+
 
 @Module({
   imports: [UserModule,
@@ -12,6 +15,9 @@ import { UserModule } from '../user/user.module';
       signOptions: {
         expiresIn: '12h'
       }
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
     })
   ],
   controllers: [AuthController],
