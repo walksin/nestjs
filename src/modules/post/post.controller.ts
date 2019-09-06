f725f92f875af9ef3a9b5ea1738893e13106432c
longfan <longfan@dayuwuxian.com>
2019-09-06 14:39:18 +0800

    [320] Save user when send post

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -3,6 +3,8 @@ import { PostService } from './post.service';
 import { async } from 'rxjs/internal/scheduler/async';
 import { PostDto } from './post.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { User as UserEntity } from '../user/user.entity';
+import { User } from '../../core/decorators/user.decorator';
 
 
 @Controller('posts')
@@ -13,8 +15,8 @@ export class PostController {
 
     @Post()
     @UseGuards(AuthGuard('jwt'))
-    async store(@Body() data: PostDto) {
-        return await this.postService.store(data)
+    async store(@Body() data: PostDto, @User() user: UserEntity) {
+        return await this.postService.store(data, user)
     }
 
     @Get()
