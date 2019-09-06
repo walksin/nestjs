da9f78a67bf5cb5266023f364497c9b9eba1be55
longfan <longfan@dayuwuxian.com>
2019-09-06 14:43:24 +0800

    [321] Join table

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert, BeforeUpdate, OneToMany } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert, BeforeUpdate, OneToMany, ManyToMany, JoinTable } from 'typeorm'
 import * as bcrypt from 'bcrypt'
 import { Exclude } from 'class-transformer';
 import { Post } from '../post/post.entity';
@@ -24,6 +24,10 @@ export class User {
 
     @OneToMany(type => Post,post => post.user)
     posts:Post[]
+
+    @ManyToMany(type => Post)
+    @JoinTable()
+    votes:Post[]
 
     @BeforeInsert()
     @BeforeUpdate()
